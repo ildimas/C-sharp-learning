6139569592b13e25b63acdb4dd8c50ea357de25c
Ilyushchenya <dimailyushen@gmail.com>
2022-11-20 22:05:40 +0300

    lesson 2

--- lesson2(constructions)/Program.cs
+++ lesson2(constructions)/Program.cs
@@ -2,7 +2,7 @@
 {
     static void Main(string[] args)
     {
-        //! 1) пример программы без условных конструкций
+        //! пример программы без условных конструкций
         // {
         //     Console.WriteLine("Введите первое слогаемое: ");
         //     int a = Convert.ToInt32(Console.ReadLine());
@@ -12,7 +12,7 @@
 
         //     Console.WriteLine($"{a} + {b} = {a + b}");
         // }  
-        //! 2) условный оператор 
+        //!  условный оператор 
         // {
         //     Console.WriteLine("Сегодня солнечно Y/N/NS");
         //     var user_choise = Console.ReadLine();
@@ -30,19 +30,103 @@
         //     }
         // }  
         //! Операторы сравнения
+        // {
+        //     int a = 5, b = 6;
+        //     Console.WriteLine($"{a} == {b} ? - " + (a == b));
+        //     Console.WriteLine($"{a} != {b} ? - " + (a != b));
+        //     Console.WriteLine($"{a} > {b} ? - " + (a > b));
+        //     Console.WriteLine($"{a} < {b} ? - " + (a < b));
+
+        //     //! работа условного оператора с типом данных string
+        //     string name = "Александр";
+        //     Console.WriteLine($"Тебя зовут Александр ? - " + (name == "Александр"));
+        //     Console.WriteLine($"Тебя зовут Гоша ? - " + (name == "Гоша"));
+
+        //     //! Тест на совершеннолетик
+        //     Console.WriteLine("Введите свой возраст: ");
+        //     int age = int.Parse(Console.ReadLine());
+        //     if (age < 18)
+        //     {
+        //         Console.WriteLine("Вы несовершеннолетний");
+        //     }
+        //     else
+        //     {
+        //         Console.WriteLine("Вы совершеннолетний");
+        //     }
+        //! Логические операторы
+        // {
+        //     bool first = true;
+        //     bool second = false;
+        //     //! оператор И 
+        //     Console.WriteLine($"{first && second}");
+        //     //! оператор ИЛИ
+        //     Console.WriteLine($"{first || second}");
+        //     //! оператор отрицания
+        //     Console.WriteLine("Введите число");
+        //     int i = int.Parse(Console.ReadLine());
+        //     bool isOdd = (i % 2 == 0);
+        //     bool isEven = !isOdd;
+        //     if (isOdd == true)
+        //     {
+        //         Console.WriteLine($"Число {i} является четным");
+        //     }
+        //     else if (isEven != false)
+        //     {
+        //         Console.WriteLine($"Число {i} является нечетным");
+        //     }
+        // }
+        //! Тернарный оператор
+        // {
+        //     int a = 10;
+        //     int b = 1;
+            
+        //     int c = b == 0
+        //         ? 1
+        //         : a / b;
+        //     Console.WriteLine(c);
+        // }
+        //! Шаблон switch
+        // {
+        //     Console.WriteLine("Введите число: ");
+        //     int num = int.Parse(Console.ReadLine());
+        //     switch(num)
+        //     {
+        //         case 1:
+        //         num += 1;
+        //         Console.WriteLine("case 1");
+        //         break;
+
+        //         case 2:
+        //         num += 2;
+        //         Console.WriteLine("case 2");
+        //         break;
+
+        //         case 3:
+
+        //         default:
+        //             Console.WriteLine("Default case");
+        //             break;
+                
+        //     }
+        // }
+        //! switch expression
         {
-            int a = 5, b = 6;
-            Console.WriteLine($"{a} == {b} ? - " + (a == b));
-            Console.WriteLine($"{a} != {b} ? - " + (a != b));
-            Console.WriteLine($"{a} > {b} ? - " + (a > b));
-            Console.WriteLine($"{a} < {b} ? - " + (a < b));
+            Console.WriteLine("Введите число от 0 до 15");
+            int a = int.Parse(Console.ReadLine());
 
-            //! работа условного оператора с типом данных string
-            string name = "Александр";
-            Console.WriteLine($"Тебя зовут Александр ? - " + (name == "Александр"));
-            Console.WriteLine($"Тебя зовут Гоша ? - " + (name == "Гоша"));
-
+            string b = a switch
+            {
+                > 0 and < 10 => Convert.ToString(a),
+                10 => "A",
+                11 => "B",
+                12 => "C",
+                13 => "D",
+                14 => "E",
+                15 => "F",
+                _ => "Мы не умеем переводить такие числа"
+            };
+            Console.WriteLine(b);
         }
-
     }
+
 }
